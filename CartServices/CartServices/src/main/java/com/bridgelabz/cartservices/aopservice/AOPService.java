a5f40c1b59b84a1c416613d1928a8e22918de723
KishlayKishan <kishan.kishlay20@gmail.com>
2022-06-11 13:09:46 +0530

    Added:AOP for Cart-Service

---
+++ CartServices/CartServices/src/main/java/com/bridgelabz/cartservices/aopservice/AOPService.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.cartservices.aopservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class AOPService {
+
+
+    @Before("execution(* com.bridgelabz.cartservices.controller.CartController.*(..))")
+    public void logBeforeV1(JoinPoint joinPoint) {
+        log.info("Initiating API : " + joinPoint.getSignature().getName() + " ");
+    }
+
+
+    @After("execution(* com.bridgelabz.cartservices.controller.CartController.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        log.info("API successfully Executed : " + joinPoint.getSignature().getName() + " ");
+    }
+
+
+    @After("execution(* com.bridgelabz.cartservices.controller.CartController.*(..))")
+    public void logAfterAndSaveArgs(JoinPoint joinPoint) {
+        log.info("After : " + Arrays.toString(joinPoint.getArgs()));
+    }
+}
